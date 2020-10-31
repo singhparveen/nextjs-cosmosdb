a309750a860187836266c0aec0eb181017cb4692
Parveen Singh <singh_parveen@outlook.com>
2020-10-30 20:33:01 -0700

    Adding Index and Cosmos Connection

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {};
+
+config.endpoint = "https://nextjscosmos.documents.azure.com:443/";
+config.key =  "suTfF76Dds3SbFbJf0204qoHkwRVRFelIkVLOoxc9vbynpt4puS7pygLKignzS1ZIxhgwNIm9UYYBI2Fz7zWwA==";
+config.database = "ToDoList";
+config.container = "Items";
+
+module.exports = config;
