89a7a730fbd3f7428740dc5ff23630754350bd40
Parveen Singh <singh_parveen@outlook.com>
2020-11-16 16:23:33 -0800

    Updated Headers and Design Change

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 const CosmosClient = require("@azure/cosmos").CosmosClient;
 import config from "../config";
+import Head from "next/head";
 
 Home.getInitialProps = async function () {
   const { endpoint, key, database, container } = config;
@@ -25,30 +26,79 @@ Home.getInitialProps = async function () {
 export default function Home({ CosmoData }) {
   return (
     <div>
-      <div className="text-3xl flex mx-2 md:mx-auto my-10 max-w-2xl">HomePage</div>
-      {CosmoData.map(({ id, name, category, description, isComplete }) => (
-        <div
-          className="flex bg-white shadow-lg rounded-lg mx-2 md:mx-auto my-10 max-w-2xl"
-          key={id}
-        >
-          <div className="flex items-start px-4 py-6">
-            <div className="">
-              <div className="inline items-center justify-between">
-                <h2 className="text-lg font-semibold text-gray-900 -mt-1">
-                  {name}
-                </h2>
-                <small className="text-sm text-gray-700 object-right">
-                  Category: {category}
-                </small>
-                <small className="ml-3 text-gray-700 text-sm">
-                  Status: {isComplete}
-                </small>
+      <Head>
+        <title>Next.JS with Azure Cosmos DB</title>
+        <meta name="title" content="Next.JS with Azure Cosmos DB" />
+        <meta
+          name="description"
+          content="Next JS Application with Azure Cosmos DB Connection"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://nextcosmo.parveensingh.com/" />
+        <meta property="og:title" content="Next.JS with Azure Cosmos DB" />
+        <meta
+          property="og:description"
+          content="Next JS Application with Azure Cosmos DB Connection"
+        />
+        <meta
+          property="og:image"
+          content="%PUBLIC_URL%/AppService-CosmosNext.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://nextcosmo.parveensingh.com/"
+        />
+        <meta property="twitter:title" content="Next.JS with Azure Cosmos DB" />
+        <meta
+          property="twitter:description"
+          content="Next JS Application with Azure Cosmos DB Connection"
+        />
+        <meta
+          property="twitter:image"
+          content="%PUBLIC_URL%/AppService-CosmosNext.png"
+        />
+      </Head>
+      <div className="w-full text-center bg-blue-800 flex flex-wrap items-center">
+        <div className="text-3xl w-1/2 text-white mx-2 md:mx-auto py-10">
+          NextJS - CosmosDB Sample
+        </div>
+        <div className="w-1/2">
+          <a
+            className="p-2 rounded text-white border-white border hover:bg-orange-700 hover:text-gray-100"
+            href="https://parveensingh.com/next-js-node-app-with-cosmos-db/"
+          >
+            Visit the Blog
+          </a>
+        </div>
+      </div>
+      <div className="bg-gray-200 py-10">
+        {CosmoData.map(({ id, name, category, description, isComplete }) => (
+          <div
+            className="flex bg-white shadow-lg rounded-lg mx-2 md:mx-auto mb-10 max-w-2xl"
+            key={id}
+          >
+            <div className="flex items-start px-4 py-6">
+              <div className="">
+                <div className="inline items-center justify-between">
+                  <h2 className="text-lg font-semibold text-gray-900 -mt-1">
+                    {name}
+                  </h2>
+                  <small className="text-sm text-gray-700 object-right">
+                    Category: {category}
+                  </small>
+                  <small className="ml-3 text-gray-700 text-sm">
+                    Status: {isComplete}
+                  </small>
+                </div>
+                <p className="mt-3 text-gray-700 text-sm">{description}</p>
               </div>
-              <p className="mt-3 text-gray-700 text-sm">{description}</p>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
